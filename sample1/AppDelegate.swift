a2631444d8c9d033bb30a2e3220bc8b9ba8b105c
John Guillory <kf5qeo@mac2.local>
2014-12-08 17:24:10 -0600

    Initial Commit

---
+++ sample1/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  sample1
+//
+//  Created by John Guillory on 12/8/14.
+//  Copyright (c) 2014 John Guillory. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ sample1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  sample1
+//
+//  Created by John Guillory on 12/8/14.
+//  Copyright (c) 2014 John Guillory. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
